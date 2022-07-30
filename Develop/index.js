9205b92d7a4387218edbb7d444d55d0d89e917ae
paullsshin <paullsshin@hotmail.com>
2022-07-30 08:31:50 -0700

    added js files and readme

---
+++ Develop/index.js
@@ -0,0 +1,41 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateReadMe = require("./utils/generateMarkdown")
+
+//  write fle
+const fs = require("fs");
+
+// asked the user for data
+const inquirer = require("inquirer");
+
+// generate the html
+const generateMd = require("./utils/generateMarkdown");
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    // {
+    //     type:
+    //     message:
+    //     name: 
+    // }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+    // ask the user questions
+    // THEN I want to pass 'responses' to generateMarkDown so we can get the 'markdown'
+
+    // THEN write the markdown' to a file using the 'writeToFile' other than README.md
+    // put the file name in the .gitignore as well
+    // 
+}
+
+// Function call to initialize app
+init();
